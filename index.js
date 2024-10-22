2c0bc6d7d4a9377415e88dc8022011cad9fced1b
raman <ramancdry2058@gmail.com>
2024-10-22 22:05:56 +0545

    changes script

--- index.js
+++
@@ -1,8 +0,0 @@
-// const r='raman'
-// console.log(r)
-// const Add=require('./add')
-import Add from './add.js'
-
-const add=2;
-const value=Add(add);
-console.log(value);
--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
   //JSON =>JavaScript Object Notation
   res.status(500).json({
     appName: "shopfiyGhar",
-    version:"1.0.0",
+    version:"2.0.0",
     port:PORT,
   });
 });
