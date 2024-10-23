93750c344964116d9dbfb10a26fe49635e557801
raman <ramancdry2058@gmail.com>
2024-10-23 09:10:46 +0545

    changes cors policy

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ dotenv.config();
 connectDB();
 app.use(cookieParser());
 app.use(cors({
-  origin:"http://localhost:3000"
+  origin:"https://shopfiyghar.vercel.app"
 }))
 const PORT=process.env.PORT;
 
