61b70ccdb9bb28ffd76b37c01d9e31ffb2bdef22
raman <ramancdry2058@gmail.com>
2025-02-16 16:26:05 +0545

    getuser and contact addd

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,11 @@
 import { userRegister,userLog,logout, getAllUser} from "../controllers/userController.js";
 import express from "express";
+import auth from "../middlewares/auth.js";
+import roleBaseAuth from "../middlewares/roleBasedAuth.js";
 const router=express.Router();
 
 router.post("/register",userRegister);
 router.post("/login",userLog);
 router.post("/logout",logout);
-router.get("/users",getAllUser)
+router.get("/users",[auth,roleBaseAuth("ADMIN")],getAllUser)
 export default router;
