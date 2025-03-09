5f7e8f4c53a83eca2297a1b7d3409431f2494c0d
raman <ramancdry2058@gmail.com>
2025-03-09 18:40:46 +0545

    added header content-Typle,Authorization)

--- src/app.js
+++ src/app.js
@@ -19,19 +19,11 @@ dotenv.config();
 connectDB();
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.APP_URL || "http://localhost:3000"
+  origin: process.env.APP_URL || "http://localhost:3000", // Allow frontend
+  methods: "GET, POST, PUT, DELETE, OPTIONS",
+  allowedHeaders: "Content-Type, Authorization",
+  credentials: true,
 }));
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.APP_URL || "http://localhost:3000");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200); // Handle preflight requests
-  }
-  next();
-});
 
 
 
