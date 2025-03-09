b7bf47855673cc29532013d02f079e31d4c2768f
raman <ramancdry2058@gmail.com>
2025-03-09 18:23:02 +0545

    added header content-Typle,Authorization)

--- src/app.js
+++ src/app.js
@@ -18,6 +18,13 @@ app.use(bodyParser.json());
 dotenv.config();
 connectDB();
 app.use(cookieParser());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 
 app.use(cors({
   //  origin:"https://shopfiyghar.vercel.app"
